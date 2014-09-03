ee808d658246b2919be19a52c9b04fd4b84b7adc
Dylan Peerenboom & Andrew M Westling <student@epicodus.com>
2014-09-03 16:41:11 -0700

    begin to add inline recursive commenting WIP

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -31,7 +31,7 @@ class CommentsController < ApplicationController
 
   def edit
     @comment = Comment.find(params[:id])
-    @article = Article.find(params[:id])
+    @article = Article.find(params[:article_id])
   end
 
   private
