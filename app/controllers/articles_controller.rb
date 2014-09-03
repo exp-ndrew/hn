d0b12fdbc889c0ce04e0ad5ba0ee65d8ebc35866
Dylan Peerenboom & Andrew M Westling <student@epicodus.com>
2014-09-03 12:15:08 -0700

    add new and show routes/views for Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,27 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create(article_params)
+    flash[:notice] = "Article added."
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :content, :link)
+    end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,8 @@
 class Article < ActiveRecord::Base
   belongs_to :user
   has_many :comments, :as => :commentable
-  validates :description, :presence => true
+  validates :content, :presence => true
+  validates :title, :presence => true
   validates :votes, :presence => true
   # validates :user_id, :presence => true
 end
